dd9109dc76fbf679faf7e3c37699dfe4ad4ab0a8
Vicente <pouyio@gmail.com>
2018-03-25 20:57:55 +0200

    fixed log paths

--- src/scrapper/logger.js
+++ src/scrapper/logger.js
@@ -1,6 +1,6 @@
 const SimpleNodeLogger = require('simple-node-logger');
 
 module.exports = SimpleNodeLogger.createSimpleLogger({
-    logFilePath: process.env.LOG_PATH,
+    logFilePath: process.env.PWD + process.env.LOG_PATH,
     timestampFormat: 'YYYY-MM-DD HH:mm:ss',
   });
