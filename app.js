9aedee721b2381e08d2dd6e9353ddb0ce38b7b84
Vicente <pouyio@gmail.com>
2018-03-25 21:48:36 +0200

    moved to http2, huge bump!

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ if (process.env.NODE_ENV === 'production') {
         key: fs.readFileSync('/etc/letsencrypt/live/aws/privkey.pem'),
         cert: fs.readFileSync('/etc/letsencrypt/live/aws/fullchain.pem')
     }
-    require('https')
+    require('spdy')
         .createServer(options || {}, api)
         .listen(process.env.PORT, () => console.log('Comics-api listenin on port ' + process.env.PORT));
-
     new CronJob('0 0 5 * * *', scrap, null, true);
 } else {
-    
+
     require('http')
-    .createServer(api)
-    .listen(process.env.PORT, () => console.log('Comics-api listenin on port ' + process.env.PORT));
+        .createServer(api)
+        .listen(process.env.PORT, () => console.log('Comics-api listenin on port ' + process.env.PORT));
 }
