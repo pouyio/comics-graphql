116b427eb07f715e6d39037a42730c2b16655024
Vicente <pouyio@gmail.com>
2018-12-31 19:46:03 +0100

    fixed user-agent

--- src/source/index.js
+++ src/source/index.js
@@ -4,7 +4,14 @@ const makeRequest = (url, img) => {
 
     return new Promise((resolve, reject) => {
 
-        const options = { method: 'GET', url, followAllRedirects: true };
+        const options = {
+            url,
+            method: 'GET',
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+            },
+            followAllRedirects: true
+        };
 
         if (url.includes('/Uploads/') || img) {
             options.encoding = null;
