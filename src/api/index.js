40a3d11919b4918be706f4e0a4165c4d234ddbcc
Vicente <pouyio@gmail.com>
2019-01-01 01:30:45 +0100

    trying netlify

--- src/api/index.js
+++ src/api/index.js
@@ -1,23 +1,28 @@
 const express = require('express');
 const compression = require('compression');
 const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
 const { ApolloServer, AuthenticationError } = require('apollo-server-express');
 const cors = require('./cors');
 const { typeDefs, resolvers } = require('./schema');
 const { get_user_logged, login } = require('./auth');
 const { img_proxy_limiter, img_proxy_cache, img_proxy, img_download } = require('./img');
 
+const BASE_URL_DEPLOY = '/.netlify/functions/index';
+
 const api = express();
+const router = express.Router();
 
 api.use(compression());
 api.use(cors);
 api.use(bodyParser.json());
 
-api.post('/login', login);
+router.get('/', (req, res) => res.send('ok!'));
+router.post('/login', login);
 
-api.get('/img/*', img_proxy_cache, img_proxy_limiter, img_proxy);
+router.get('/img/*', img_proxy_cache, img_proxy_limiter, img_proxy);
 
-api.get('/proxy-img/*', img_download);
+router.get('/proxy-img/*', img_download);
 
 const server = new ApolloServer({
     typeDefs,
@@ -25,9 +30,10 @@ const server = new ApolloServer({
     context: async ({ req, res }) => {
         const user = await get_user_logged(req, res);
         return { user };
-    },
-    playground: { endpoint: `http://localhost:4000/graphql` }
+    }
 });
-server.applyMiddleware({ app: api, path: '/graphql' });
+server.applyMiddleware({ app: api, path: BASE_URL_DEPLOY + '/graphql' });
 
-module.exports = { api };
+api.use(BASE_URL_DEPLOY, router);
+
+module.exports.handler = serverless(api);
