6757b88491b1fc4e0daf32c07c9ea132d5297f3d
Vicente <pouyio@gmail.com>
2019-01-01 20:15:37 +0100

    img test - nothing

--- src/api/img.js
+++ src/api/img.js
@@ -1,44 +1,44 @@
-const AWS = require('aws-sdk');
-const RateLimit = require('express-rate-limit');
+// const AWS = require('aws-sdk');
+// const RateLimit = require('express-rate-limit');
 // const { makeRequest } = require('../source');
 
-const s3 = new AWS.S3({
-    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY
-})
+// const s3 = new AWS.S3({
+//     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY
+// })
 
-const _findInBucket = (filename) => {
-    return new Promise((resolve) => {
-        s3.headObject({
-            Bucket: process.env.BUCKET_NAME,
-            Key: filename
-        }, (err, data) => {
-            if (err || !data) {
-                resolve();
-            } else {
-                resolve(data);
-            }
-        });
-    })
-}
-const _saveToBucket = (filename, data, type) => {
-    return new Promise((resolve) => {
-        s3.putObject({
-            Bucket: process.env.BUCKET_NAME,
-            Key: filename,
-            Body: data,
-            ACL: 'public-read',
-            ContentType: type
-        }, resolve);
-    })
-}
+// const _findInBucket = (filename) => {
+//     return new Promise((resolve) => {
+//         s3.headObject({
+//             Bucket: process.env.BUCKET_NAME,
+//             Key: filename
+//         }, (err, data) => {
+//             if (err || !data) {
+//                 resolve();
+//             } else {
+//                 resolve(data);
+//             }
+//         });
+//     })
+// }
+// const _saveToBucket = (filename, data, type) => {
+//     return new Promise((resolve) => {
+//         s3.putObject({
+//             Bucket: process.env.BUCKET_NAME,
+//             Key: filename,
+//             Body: data,
+//             ACL: 'public-read',
+//             ContentType: type
+//         }, resolve);
+//     })
+// }
 
-const img_proxy_limiter = new RateLimit({
-    windowMs: 5 * 1000,
-    delayAfter: 5,
-    max: 0,
-    delayMs: 5 * 1000
-});
+// const img_proxy_limiter = new RateLimit({
+//     windowMs: 5 * 1000,
+//     delayAfter: 5,
+//     max: 0,
+//     delayMs: 5 * 1000
+// });
 
 const img_proxy = async (req, res) => {
     const filename = req.params['0'];
@@ -49,41 +49,41 @@ const img_proxy = async (req, res) => {
         res.header('Content-Type', type);
         res.header('Content-Length', body.length);
         res.end(body);
-        _saveToBucket(filename, body, type);
+        // _saveToBucket(filename, body, type);
     } catch (err) {
         console.log(err);
         res.end();
     }
 }
 
-const img_download = async (req, res) => {
-    const url = `${req.params['0']}`;
-    try {
-        // const { body, type } = await makeRequest(url, true);
-        const { body, type } = {};
-        const img = new Buffer.from(body, 'base64');
-        res.header('Content-Type', type);
-        res.header('Content-Length', img.length);
-        res.end(img);
-    } catch (err) {
-        console.log(err);
-        res.end();
-    }
-}
+// const img_download = async (req, res) => {
+//     const url = `${req.params['0']}`;
+//     try {
+//         // const { body, type } = await makeRequest(url, true);
+//         const { body, type } = {};
+//         const img = new Buffer.from(body, 'base64');
+//         res.header('Content-Type', type);
+//         res.header('Content-Length', img.length);
+//         res.end(img);
+//     } catch (err) {
+//         console.log(err);
+//         res.end();
+//     }
+// }
 
-const img_proxy_cache = async (req, res, next) => {
-    const filename = req.params['0'];
-    const data = await _findInBucket(filename);
-    if (data) {
-        res.redirect(`https://s3.eu-central-1.amazonaws.com/${process.env.BUCKET_NAME}/${filename}`);
-    } else {
-        next();
-    }
-}
+// const img_proxy_cache = async (req, res, next) => {
+//     const filename = req.params['0'];
+//     const data = await _findInBucket(filename);
+//     if (data) {
+//         res.redirect(`https://s3.eu-central-1.amazonaws.com/${process.env.BUCKET_NAME}/${filename}`);
+//     } else {
+//         next();
+//     }
+// }
 
 module.exports = {
-    img_proxy_limiter,
-    img_proxy,
-    img_download,
-    img_proxy_cache
+    // img_proxy_limiter,
+    // img_download,
+    // img_proxy_cache
+    img_proxy
 }
