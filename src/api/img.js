03c5fdfac567770bdacdfb9d7dc2992b09182753
Vicente <pouyio@gmail.com>
2019-01-04 11:48:44 +0100

    fix2? error decoding lambda response: json: cannot unmarshal number into Go value of type string

--- src/api/img.js
+++ src/api/img.js
@@ -45,10 +45,8 @@ const img_proxy = async (req, res) => {
     const url = `${process.env.SOURCE_URL}${filename}`;
     try {
         const { body, type } = await makeRequest(url);
-        res.header('Content-Type', type);
-        res.header('Content-Length', '' + body.length);
-        res.end(body);
-        _saveToBucket(filename, body, type);
+        await _saveToBucket(filename, body, type);
+        res.redirect(`https://s3.eu-central-1.amazonaws.com/${process.env.BUCKET_NAME}/${filename}`);
     } catch (err) {
         console.log(err);
         res.end();
