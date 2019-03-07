627335c19dc3c3cd090afb43b7ac902ac34e4bc0
Vicente <pouyio@gmail.com>
2019-03-07 21:56:40 +0100

    removed unused dep

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// require('dotenv').load();
-// const serverless = require('serverless-http');
 const { app } = require('./api');
 
 module.exports = app;
-// module.exports.handler = serverless(app, { binary: ['image/*'] });
