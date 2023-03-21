b4b22b39424ef72fb86e41ed61d282920659b8b8
pouyio <pouyio@gmail.com>
2023-03-21 23:00:39 +0100

    testing railway

--- src/scrapper/job.js
+++ src/scrapper/job.js
@@ -1,7 +1,4 @@
-require("dotenv").load();
 const cron = require("node-cron");
 const { scrap } = require("./index");
 
-cron.schedule(`30 21 * * *`, async () => {
-  scrap();
-});
+cron.schedule(`10 0 * * *`, async () => {});
